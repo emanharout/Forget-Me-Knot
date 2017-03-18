2fab6ff41c4b161d19976f58595ad5b2e90892bf
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-03-18 15:17:32 -0700

    Squashed commit of the following:
    commit f0b74e1e13e6df756b201961584562cca3723f5c
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Sat Mar 18 13:34:45 2017 -0700
    
        Refactor: create helper methods for view setup
    
    commit 751ee826a7041dd0e122bff82248fe3eeda94937
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Sat Mar 18 13:31:59 2017 -0700
    
        Add attributes to bar button item text
    
    commit 82166dcd93553e4d5169c9c9423fe1434ad24bc4
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Sat Mar 18 01:55:09 2017 -0700
    
        Setup navigation bar
    
    commit 6841e00556e6a0bd275d448b39c02751025cc93c
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Sat Mar 18 01:17:57 2017 -0700
    
        Organize files into folders
    
    commit a98fe81a3204b47e6fd4c5eac55860ae33768690
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Sat Mar 18 01:14:53 2017 -0700
    
        Setup tableView delegation and dataSource methods
    
    commit cce117725b603e9ca62b93f87be1fd4c7cfed821
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Sat Mar 18 01:02:52 2017 -0700
    
        Add remaining UI elements
    
    commit 63aed8bffda64311e54193c149a3bd050763f07d
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 23:35:42 2017 -0700
    
        Setup flowLayout properties
    
    commit 48b87369f6994bda5d7036a6eca00f557c810d6c
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 23:27:45 2017 -0700
    
        Create custom cell TabCell
    
    commit 73037cb7a01734cda1ea847972e7fcf2f6b9b8ae
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 23:07:38 2017 -0700
    
        Create collection view and setup delegation protocols
    
    commit 3639cdf32b60d4ab40178b0c48a540fdf162db07
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 22:51:36 2017 -0700
    
        Remove statusbar from entire app
    
    commit f352d336e5fdefcdc9ea30ea872238dfadfe9e13
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 22:43:13 2017 -0700
    
        Embed GroceryListsViewController into NavigationController
    
    commit f505e0fe47c77cfdddbbd00fe8808b85ae197cf5
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 22:42:04 2017 -0700
    
        Create noListsStackView
    
    commit 66871dbfb4528fb924f5ff06cb7af54299bc9f06
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 21:15:11 2017 -0700
    
        Create GroceryListsViewController.swift file and hookup to storyboard
    
    commit 612718b0415c30492f9f836ba046da7e6f1b5c9b
    Author: Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
    Date:   Fri Mar 17 21:11:39 2017 -0700
    
        Create .gitignore file

--- Forget Me Knot/GroceryListsViewController.swift
+++ Forget Me Knot/GroceryListsViewController.swift
@@ -14,13 +14,37 @@ class GroceryListsViewController: UIViewController {
   @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
   @IBOutlet weak var listNameLabel: UILabel!
   @IBOutlet weak var listDescriptionLabel: UILabel!
+  @IBOutlet weak var newListBarButtonItem: UIBarButtonItem!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    setupViews()
+  }
+  
+  func setupViews() {
+    setupFlowLayout()
+    setupNavigationBar()
+  }
+  
+  func setupFlowLayout() {
     flowLayout.scrollDirection = .horizontal
     flowLayout.estimatedItemSize = CGSize(width: 100, height: 50)
     flowLayout.sectionInset = UIEdgeInsets(top: 8, left: 12, bottom: 8, right: 12)
+  }
+  
+  func setupNavigationBar() {
+    if let logoImage = UIImage(named: "Logo") {
+      let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 36, height: 34))
+      imageView.image = logoImage
+      imageView.contentMode = .scaleAspectFit
+      navigationItem.titleView = imageView
+    }
+    
+    let font = UIFont(name: "Avenir-Light", size: 22)
+    let attributes = [NSFontAttributeName: font!,
+                      NSKernAttributeName : CGFloat(10.0)] as [String : Any]
+    newListBarButtonItem.setTitleTextAttributes(attributes, for: .normal)
   }
 
 }
