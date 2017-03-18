82166dcd93553e4d5169c9c9423fe1434ad24bc4
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-03-18 01:55:09 -0700

    Setup navigation bar

--- Forget Me Knot/GroceryListsViewController.swift
+++ Forget Me Knot/GroceryListsViewController.swift
@@ -21,6 +21,13 @@ class GroceryListsViewController: UIViewController {
     flowLayout.scrollDirection = .horizontal
     flowLayout.estimatedItemSize = CGSize(width: 100, height: 50)
     flowLayout.sectionInset = UIEdgeInsets(top: 8, left: 12, bottom: 8, right: 12)
+    
+    if let logoImage = UIImage(named: "Logo") {
+      let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 36, height: 34))
+      imageView.image = logoImage
+      imageView.contentMode = .scaleAspectFit
+      navigationItem.titleView = imageView
+    }
   }
 
 }
