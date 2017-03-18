ad50d6d059ca597be8cb0302bcd210d55c0753f0
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-03-18 15:20:34 -0700

    Create GroceryList and Item models

---
+++ Forget Me Knot/GroceryList.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryList.swift
+//  Forget Me Knot
+//
+//  Created by Emmanuoel Haroutunian on 3/18/17.
+//  Copyright © 2017 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import Foundation
+
+struct GroceryList {
+  
+  let name: String
+  let description: String
+  let items: [Item]
+
+}
