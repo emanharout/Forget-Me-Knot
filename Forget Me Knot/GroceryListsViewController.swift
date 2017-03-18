66871dbfb4528fb924f5ff06cb7af54299bc9f06
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-03-17 21:15:11 -0700

    Create GroceryListsViewController.swift file and hookup to storyboard

---
+++ Forget Me Knot/GroceryListsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Forget Me Knot
+//
+//  Created by Emmanuoel Haroutunian on 3/17/17.
+//  Copyright © 2017 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class GroceryListsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
+
