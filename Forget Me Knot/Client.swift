a4193e910df6f8c58f1c9bf0c51f3df9d37b4198
Emmanuoel Haroutunian <emmanuoel.h@gmail.com>
2017-03-19 10:13:57 -0700

    Add upload(groceryList: completionHandler:) function to Client

--- Forget Me Knot/Client.swift
+++ Forget Me Knot/Client.swift
@@ -48,4 +48,53 @@ class Client {
     task.resume()
   }
   
+  func upload(groceryList: GroceryList, completionHandler: @escaping (_ result: Any?, _ error: Error?)->Void) {
+    guard let url = URL(string: "https://forget-me-knot-api.herokuapp.com/api/v1/grocery_lists.json") else { return }
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("haroutunian_1989", forHTTPHeaderField: "Authorization")
+    
+    let items = groceryList.items
+    var itemsJSONArray = " ["
+    for item in items {
+      let itemJSONString = "{\"item_id\": \(item.id)},"
+      itemsJSONArray.append(itemJSONString)
+    }
+    itemsJSONArray.remove(at: itemsJSONArray.index(before: itemsJSONArray.endIndex))
+    itemsJSONArray.append("]")
+    print(itemsJSONArray)
+    
+    let body = "{\"grocery_list\": {\"name\": \"\(groceryList.name)\",\"description\": \"\(groceryList.description)\",\"list_items_attributes\": \(itemsJSONArray)}}"
+    request.httpBody = body.data(using: .utf8)
+    
+    
+    let session = URLSession.shared
+    let task = session.dataTask(with: request) { (data, response, error) in
+      
+      guard error == nil else {
+        completionHandler(nil, error)
+        return
+      }
+      
+      guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode < 300 else {
+        print("Non 2xx status code from server")
+        return
+      }
+      
+      guard let data = data else {
+        print("Couldn't retrieve data from server")
+        return
+      }
+      
+      do {
+        let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        completionHandler(json, nil)
+      } catch {
+        completionHandler(nil, error)
+      }
+    }
+    task.resume()
+  }
+  
 }
